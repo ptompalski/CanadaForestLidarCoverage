539839c194ea5041957a0e6b8ff91d6b277f20ac
Piotr Tompalski <piotr.tompalski@gmail.com>
2024-09-20 10:14:53 -0700

    updates to index and maps

--- copy_site_to_networkFolder.bat
+++ copy_site_to_networkFolder.bat
@@ -1,2 +1,2 @@
-xcopy /Y /e "C:\Users\ptompals\OneDrive - NRCan RNCan\__WORK\CanadaForestryLidar\_site" "\\vic-fas1\france\cwfc\forest_inventory_status\_site"
+xcopy /Y /e "C:\Users\ptompals\OneDrive - NRCan RNCan\__WORK\CanadaForestLidarCoverage\_site" "\\vic-fas1\france\cwfc\forest_inventory_status\_site"
 pause
