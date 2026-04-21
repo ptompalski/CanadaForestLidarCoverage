5ace29efe48d439a786ee36f16e641ae0970cea6
Piotr Tompalski <piotr.tompalski@gmail.com>
2026-04-21 12:33:55 -0700

    Refactor BC preprocessing spatial helpers

--- R/1001_preprocess_BC.R
+++ R/1001_preprocess_BC.R
@@ -4,7 +4,18 @@ source("R/0000_setup.R")
 
 #newest updates in the bottom
 
-dir_create("layers/pre-processed/BC")
+bc_output_dir <- Sys.getenv("BC_OUTPUT_DIR", unset = "layers/pre-processed/BC")
+bc_output_file <- Sys.getenv(
+  "BC_OUTPUT_FILE",
+  unset = file.path(bc_output_dir, "ALS_BC.gpkg")
+)
+bc_output_diss_file <- Sys.getenv(
+  "BC_OUTPUT_DISS_FILE",
+  unset = file.path(bc_output_dir, "ALS_BC_diss.gpkg")
+)
+
+dir_create(dirname(bc_output_file))
+dir_create(dirname(bc_output_diss_file))
 
 bc_source_dir <- "layers/source_layers/BC"
 bc_status_pattern <- file.path(bc_source_dir, "Provincial_ALS_Status_*.shp")
@@ -22,7 +33,10 @@ bc_date_year <- function(x) {
 }
 
 read_bc_status_layer <- function(file) {
-  snapshot_stamp <- str_match(basename(file), "Provincial_ALS_Status_(\\d{8,12})\\.shp")[, 2]
+  snapshot_stamp <- str_match(
+    basename(file),
+    "Provincial_ALS_Status_(\\d{8,12})\\.shp"
+  )[, 2]
   snapshot_year <- bc_date_year(snapshot_stamp)
 
   x <- st_read(file)
@@ -42,21 +56,12 @@ read_bc_status_layer <- function(file) {
       PPM = 8
     ) %>%
     filter(!is.na(YEAR)) %>%
-    st_zm() %>%
-    st_transform(crs = the_crs) %>%
-    group_by(YEAR, PPM) %>%
-    summarize(geometry = st_union(geometry), .groups = "drop")
+    dissolve_coverage()
 }
 
 # there were multiple shapefiles for BC
-# first was sent by Chris Butson (Lidar_BC.shp) and had acquisitions starting in 2010 up to 2020
+# first starting in 2010 up to 2020
 # second was downloaded from lidar.bc portal and had acquisitions starting in 2015 up to 2021
-# also - there is a shapefile that Joanne found with 2014 acquisitions by Tolko
-# on 2024-04-16 Chris Butson sent two more layers for 2022 and 2023 acquisitions
-# on 2024-09-06 Chris Butson sent a layer with the most recent status of the lidar acquisitions
-# on 2025-07-11 new layer sent
-
-#those last two don't have any info on acquisition date or point density
 
 #combing all of them here
 
@@ -64,20 +69,11 @@ ALS_BC_lidarbc <- st_read(
   "layers/source_layers/BC/BC_LiDAR_Point_Cloud_Index.shp"
 ) #last update 2024-04-09
 ALS_BC_lidarbc %<>% select(YEAR = year_, PPM = density)
-ALS_BC_lidarbc %<>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+ALS_BC_lidarbc %<>% dissolve_coverage()
 
 ALS_BC_older <- st_read("layers/source_layers/BC/Lidar_BC.shp")
 ALS_BC_older %<>% select(YEAR, PPM = MinimumPts)
-ALS_BC_older %<>% st_make_valid()
-ALS_BC_older %<>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+ALS_BC_older %<>% dissolve_coverage(make_valid = TRUE)
 ALS_BC_older %<>% mutate(YEAR = as.numeric(YEAR))
 
 ALS_BC_tolko <- st_read(
@@ -85,29 +81,20 @@ ALS_BC_tolko <- st_read(
 )
 ALS_BC_tolko %<>%
   mutate(YEAR = 2014, PPM = 2) %>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+  dissolve_coverage()
 
 ## older ALS acquisitions (shared on 2025-11-26)
 # North Vancouver Island 2012
 BCTS_WFP_2012 <- st_read("layers/source_layers/BC/BCTS_WFP_2012.shp")
 BCTS_WFP_2012 %<>%
   mutate(YEAR = 2012, PPM = 11) %>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+  dissolve_coverage()
 
 # Cranbrook TSA 2011
 Cranbrook_TSA_2011 <- st_read("layers/source_layers/BC/faib_ssc_lidar.shp")
 Cranbrook_TSA_2011 %<>%
   mutate(YEAR = 2011, PPM = NA) %>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+  dissolve_coverage()
 
 # Merritt TSA-2016
 Merritt_TSA_2016 <- st_read(
@@ -115,36 +102,24 @@ Merritt_TSA_2016 <- st_read(
 )
 Merritt_TSA_2016 %<>%
   mutate(YEAR = 2016, PPM = NA) %>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+  dissolve_coverage()
 
 # Sunshine Coast 2019?
 
 ALS_BC_2022_bcts <- st_read("layers/source_layers/BC/bcts_lidar_2022.shp")
 ALS_BC_2022_bcts %<>% select(YEAR = YearAcquir) %>% mutate(PPM = 8)
 
-#Yes, so page 28, table 4 in the acquisition specs document:
-#https://www2.gov.bc.ca/assets/gov/data/geographic/digital-imagery/specifications_for_airborne_lidar_for_the_province_of_british_columbia_53.pdf
-#states the nominal last-return point densities for this program and they all have to meet Quality Level 2 (QL2) which is >=8pts/m2.
-#Chris
+# page 28, table 4 in the acquisition specs document:
+# https://www2.gov.bc.ca/assets/gov/data/geographic/digital-imagery/specifications_for_airborne_lidar_for_the_province_of_british_columbia_53.pdf
+# states the nominal last-return point densities for this program and they all have to meet Quality Level 2 (QL2) which is >=8pts/m2.
 
-ALS_BC_2022_bcts %<>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+ALS_BC_2022_bcts %<>% dissolve_coverage()
 
 ALS_BC_2022 <- st_read(
   "layers/source_layers/BC/lidar_2022_se_july14_finalPriority12.shp"
 )
 ALS_BC_2022 %<>% mutate(YEAR = 2022, PPM = 8)
-ALS_BC_2022 %<>%
-  st_zm() %>%
-  st_transform(crs = the_crs) %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+ALS_BC_2022 %<>% dissolve_coverage()
 
 # new ALS acquisitions from the newest provincial status snapshot.
 # When a new Provincial_ALS_Status_*.shp is added, this automatically uses it.
@@ -155,13 +130,6 @@ bc_status_file <- latest_file_by_pattern(
 )
 message("Using BC provincial ALS status layer: ", basename(bc_status_file))
 ALS_BC_status <- read_bc_status_layer(bc_status_file)
-
-# st_write(ALS_BC_2025_no_overlap, dsn = "layers/BC/ALS_BC_2025_no_overlap.gpkg", append=F)
-# plot(ALS_BC_2025_no_overlap)
-
-# ALS_BC_union <- st_union(ALS_BC_lidarbc, ALS_BC_older)
-# ALS_BC_union <- st_union(ALS_BC_union, ALS_BC_tolko)
-# st_write(ALS_BC_union, dsn = "layers/BC/ALS_BC_union.gpkg")
 
 ALS_BC <- bind_rows(
   BCTS_WFP_2012,
@@ -176,43 +144,29 @@ ALS_BC <- bind_rows(
 )
 
 #dissolve by year and PPM
-ALS_BC <- ALS_BC %>%
-  group_by(YEAR, PPM) %>%
-  summarize(geometry = st_union(geometry))
+ALS_BC <- ALS_BC %>% dissolve_coverage()
 
 
 ALS_BC <- ALS_BC %>%
-  st_make_valid() %>%
-  st_collection_extract("POLYGON") %>%
-  st_cast("MULTIPOLYGON", warn = FALSE) %>%
-  st_as_sf() %>%
+  clean_coverage_polygons() %>%
   mutate(area = st_area(geometry)) %>%
   mutate(Province = "BC") %>%
   mutate(isAvailable = 1) %>%
   relocate(Province, YEAR, PPM, area, isAvailable) %>%
   st_as_sf()
 
-st_write(ALS_BC, dsn = "layers/pre-processed/BC/ALS_BC.gpkg", append = F)
+st_write(ALS_BC, dsn = bc_output_file, append = F)
 
 # without overlaps - newest acquisition kept
 ALS_BC_diss <- remove_overlaps_by_attr(ALS_BC, "YEAR")
 
 #update area
 ALS_BC_diss <- ALS_BC_diss %>%
-  st_make_valid() %>%
-  st_collection_extract("POLYGON") %>%
-  st_cast("MULTIPOLYGON", warn = FALSE) %>%
+  clean_coverage_polygons() %>%
   mutate(area = st_area(geometry))
 
 st_write(
   ALS_BC_diss,
-  dsn = "layers/pre-processed/BC/ALS_BC_diss.gpkg",
+  dsn = bc_output_diss_file,
   append = F
 )
-
-# ALS_BC %<>%
-#   st_as_sf() %>%
-#   mutate(area = st_area(geometry)) %>%
-#   mutate(Province="BC") %>%
-#   mutate(isAvailable=1) %>%
-#   relocate(Province, YEAR, PPM, area, isAvailable)
---
+++ R/functions/spatial.R
@@ -0,0 +1,29 @@
+dissolve_coverage <- function(x, group_cols = c("YEAR", "PPM"), crs = the_crs, make_valid = FALSE) {
+  missing_cols <- setdiff(group_cols, names(x))
+
+  if (length(missing_cols) > 0) {
+    stop(
+      "Missing required coverage column(s): ",
+      paste(missing_cols, collapse = ", "),
+      call. = FALSE
+    )
+  }
+
+  if (make_valid) {
+    x <- st_make_valid(x)
+  }
+
+  x %>%
+    st_zm() %>%
+    st_transform(crs = crs) %>%
+    group_by(across(all_of(group_cols))) %>%
+    summarize(geometry = st_union(geometry), .groups = "drop")
+}
+
+clean_coverage_polygons <- function(x) {
+  x %>%
+    st_make_valid() %>%
+    st_collection_extract("POLYGON") %>%
+    st_cast("MULTIPOLYGON", warn = FALSE) %>%
+    st_as_sf()
+}
